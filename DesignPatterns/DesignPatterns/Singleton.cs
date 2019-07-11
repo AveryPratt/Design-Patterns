c55b3bac6c91edb9e421dec91b9d1b7ae7f9deb7
Avery Pratt <apratt91@gmail.com>
2019-07-11 15:15:19 -0700

    init

---
+++ DesignPatterns/DesignPatterns/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private static int _counter;
+
+        private static Singleton _instance = null;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            _counter++;
+            Console.WriteLine("Counter value: " + _counter);
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
